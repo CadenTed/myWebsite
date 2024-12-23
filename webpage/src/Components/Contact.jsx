1313c8a32715ed7f15facb4fb641c1935a2e2c6b
Caden Tedschi <cmt489@nau.edu>
2024-12-22 18:48:40 -0700

    Updated Contact margins

--- webpage/src/Components/Contact.jsx
+++ webpage/src/Components/Contact.jsx
@@ -5,7 +5,7 @@ import "../styles/contact.css";
 function Contact() {
   return (
     <div className="container mt-5" id="contact">
-      <h2 className="text-center">Contact Me</h2>
+      <h2 className="text-center top-sec">Contact Me</h2>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <form>
